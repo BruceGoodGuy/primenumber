0fa23f754b04726b298856f8c49fb34c74f20d00
KhoaNguyen <khoauth@gmail.com>
2018-02-22 15:52:15 +0700

    update 2

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PrimeNumber</title>
+	<link rel="stylesheet" type="text/css" href="design/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Orienta" rel="stylesheet">
+</head>
+<body>
+<div class="contain">
+	<div class="intro">
+		PRIME NUMBER
+	</div>
+</div>
+</body>
+</html>
